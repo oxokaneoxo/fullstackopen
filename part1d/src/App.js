e5dccb4ed12c4e3664d4ecd3b1b24b726786c377
oxokaneoxo <kane.miller@hotmail.com>
2023-03-03 14:47:16 -0800

    part 1d: 1.7 Unicafe Step2

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / (good + neutral + bad);
+  const positive = good / (good + neutral + bad);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <h1>Give Feedback</h1>
+        <div className='buttonContainer'>
+          <button onClick={() => setGood(good + 1)} >Good</button>
+          <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
+          <button onClick={() => setBad(bad + 1)} >Bad</button>
+        </div>
+      </section>
+      <section>
+        <h1>Statistics</h1>
+        <div className='statisticsContainer'>
+          <p>Good {good}</p>
+          <p>Neutral {neutral}</p>
+          <p>Bad {bad}</p>
+          <p>Total {total}</p>
+          <p>Average {average}</p>
+          <p>Positive {positive}%</p>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
