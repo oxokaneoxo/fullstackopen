86ae0ad14649b2776f2129bddb394cc2f7aeec68
oxokaneoxo <kane.miller@hotmail.com>
2023-03-03 15:06:01 -0800

    part 1d: 1.9 Unicafe Step4

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -1,5 +1,46 @@
 import { useState } from 'react'
 
+const Feedback = (props) => {
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <div className='buttonContainer'>
+        <button onClick={props.goodClick} >Good</button>
+        <button onClick={props.neutralClick} >Neutral</button>
+        <button onClick={props.badClick} >Bad</button>
+      </div>
+    </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / (good + neutral + bad);
+  const positive = good / (good + neutral + bad);
+
+  return (
+    <div>
+      {total === 0 ?
+        <p>No feedback given</p>
+        : (
+          <>
+            <h1>Statistics</h1>
+            <p>Good {good}</p>
+            <p>Neutral {neutral}</p>
+            <p>Bad {bad}</p>
+            <p>Total {total}</p>
+            <p>Average {average}</p>
+            <p>Positive {positive}%</p>
+          </>
+        )}
+
+    </div>
+  )
+}
+
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,33 +48,10 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
 
-  const total = good + neutral + bad;
-  const average = (good - bad) / (good + neutral + bad);
-  const positive = good / (good + neutral + bad);
-
-
-
   return (
     <div>
-      <section>
-        <h1>Give Feedback</h1>
-        <div className='buttonContainer'>
-          <button onClick={() => setGood(good + 1)} >Good</button>
-          <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
-          <button onClick={() => setBad(bad + 1)} >Bad</button>
-        </div>
-      </section>
-      <section>
-        <h1>Statistics</h1>
-        <div className='statisticsContainer'>
-          <p>Good {good}</p>
-          <p>Neutral {neutral}</p>
-          <p>Bad {bad}</p>
-          <p>Total {total}</p>
-          <p>Average {average}</p>
-          <p>Positive {positive}%</p>
-        </div>
-      </section>
+      <Feedback goodClick={() => setGood(good + 1)} neutralClick={() => setNeutral(neutral + 1)} badClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
