68649e7bd053b968c2abd387963777529684d855
oxokaneoxo <kane.miller@hotmail.com>
2023-03-06 19:29:50 -0800

    Part 2c: 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './app.css';
 
 import Filter from './components/Filter';
@@ -6,15 +7,21 @@ import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567", id: "Arto Hellas" },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 'Ada Lovelace' },
-    { name: 'Dan Abramov', number: '12-43-234345', id: "Dan Abramov" },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: "Mary Poppendieck" }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data)
+      })
+  }, [])
 
 
   return (
