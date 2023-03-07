56f5c466e161eb104b8da87f471a24477f555140
oxokaneoxo <kane.miller@hotmail.com>
2023-03-07 15:43:39 -0800

    part 2: 2.12-2.14: The Phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import './app.css';
 
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import personSevice from './services/personSevice';
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -12,14 +12,14 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
-
   useEffect(() => {
-    console.log("effect");
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled');
-        setPersons(response.data)
+    personSevice
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+      .catch(error => {
+        console.log(error, "Failed to fetch database of persons")
       })
   }, [])
 
@@ -43,6 +43,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons
         persons={persons}
+        setPersons={setPersons}
         newFilter={newFilter}
       />
     </div>
