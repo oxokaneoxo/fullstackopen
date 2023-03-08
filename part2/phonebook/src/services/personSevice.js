eb03e0d50d6ceb6ee55642e8144539ff36acedaf
oxokaneoxo <kane.miller@hotmail.com>
2023-03-07 16:05:03 -0800

    VS code wanted me to use async in the services

--- part2/phonebook/src/services/personSevice.js
+++ part2/phonebook/src/services/personSevice.js
@@ -1,24 +1,28 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3001/persons";
 
-const getAll = () => {
+const getAll = async () => {
     const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+    const response = await request;
+    return response.data;
 }
 
-const create = (newObject) => {
+const create = async (newObject) => {
     const request = axios.post(baseUrl, newObject)
-    return request.then(request => request.data)
+    const request_1 = await request;
+    return request_1.data;
 }
 
-const remove = (id) => {
+const remove = async (id) => {
     const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
+    const response = await request;
+    return response.data;
 }
 
-const update = (id, newObject) => {
+const update = async (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
+    const response = await request;
+    return response.data;
 }
 
 export default { getAll, create, remove, update }
