3f2d32319a4f742ee85dd94e7088b6d0808f7791
oxokaneoxo <kane.miller@hotmail.com>
2023-03-10 17:20:06 -0800

    Part 3: 3.5: Phonebook backend step5

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -2,6 +2,8 @@ const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": 1,
@@ -21,6 +23,16 @@ let persons = [
   {
     "id": 4,
     "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  },
+  {
+    "id": 5,
+    "name": "Mary poop",
+    "number": "39-23-6423122"
+  },
+  {
+    "id": 6,
+    "name": "Mary deck",
     "number": "39-23-6423122"
   }
 ]
@@ -42,6 +54,39 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return (maxId +1);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: Math.floor(Math.random() * 10000000),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 
