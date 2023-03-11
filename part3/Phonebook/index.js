6f3b5fa0e90add3e2fab4ecbfbb6ed5513ae9176
oxokaneoxo <kane.miller@hotmail.com>
2023-03-10 16:41:31 -0800

    Part 3: 3.2-3.3 Phonebook backsend Step2-3

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -1,32 +1,48 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  let currentDate = new Date
+  response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${currentDate}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(note => note.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
 
 const PORT = 3001
