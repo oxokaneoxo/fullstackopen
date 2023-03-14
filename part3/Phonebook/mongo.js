7a75d85dbbb508882e6693d9b9ebcbdf8a6a21e9
oxokaneoxo <kane.miller@hotmail.com>
2023-03-14 15:41:38 -0700

    Part 3: 3.12: Command-line database

---
+++ part3/Phonebook/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length > 3 && process.argv.length < 5) {
+    console.log('give password, name, and number as an argument. example: node mongo.js <password> <name> <number>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://kanemiller:${password}@cluster0.d1hec5o.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('person', personSchema)
+
+const person = new Person({
+    name,
+    number,
+})
+console.log();
+
+process.argv.length == 3
+    ? Person
+        .find({})
+        .then((persons) => {
+            console.log("Phonebook:");
+            persons.forEach((p) => {
+                console.log(`Name: ${p.name} Number: ${p.number}`);
+            });
+            mongoose.connection.close();
+        })
+    : person.save().then(p => {
+        console.log(`Person: ${p.name} was added to phonebook with the Number: ${p.number}`)
+        mongoose.connection.close()
+    })
