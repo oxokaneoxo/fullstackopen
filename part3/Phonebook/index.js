61091d625cc7fd5e1daa59d38a5faec8267be01e
oxokaneoxo <kane.miller@hotmail.com>
2023-03-15 16:48:38 -0700

    Give up on fixing this

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -88,7 +88,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     name: body.name,
     number: body.number,
   }
-
+  console.log(body);
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
