0a54a8a7288dcd7fa32bab9e662fa5a5c2116353
oxokaneoxo <kane.miller@hotmail.com>
2023-03-15 15:12:16 -0700

    Part 3: 3.15: Phonebook database, step3

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -44,10 +44,12 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error =>next(error))
+  
 })
 
 // const generateId = () => {
@@ -79,7 +81,7 @@ app.post('/api/persons', (request, response) => {
   // }
 
   const person = new Person({
-    //id: generateId(),
+   // id: generateId(),
     name: body.name,
     number: body.number,
   })
