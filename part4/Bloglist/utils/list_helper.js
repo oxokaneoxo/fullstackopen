842fe69b8df348de968395d44c6efced6467b86d
oxokaneoxo <kane.miller@hotmail.com>
2023-03-17 15:45:08 -0700

    4.4: helper functions and unit tests, step2

--- part4/Bloglist/utils/list_helper.js
+++ part4/Bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    const result = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    return result;
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
