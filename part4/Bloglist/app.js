f4af0c020b48b659c0738f91f047d5ee6fda269c
oxokaneoxo <kane.miller@hotmail.com>
2023-03-22 14:46:47 -0700

    4.15: bloglist expansion, step3

--- part4/Bloglist/app.js
+++ part4/Bloglist/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const http = require('http')
 const mongoose = require('mongoose')
 
@@ -21,5 +22,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
 
 module.exports = app
