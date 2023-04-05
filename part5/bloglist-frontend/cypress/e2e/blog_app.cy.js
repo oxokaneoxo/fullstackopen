3a78e34a122d9747daf3f6048ec15d1d8a7c8673
oxokaneoxo <kane.miller@hotmail.com>
2023-04-05 15:57:50 -0700

    5.22: bloglist end to end testing, step6

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -53,5 +53,47 @@ describe('Blog app', function () {
       cy.contains('submit').click()
       cy.contains('a new blog Cypress Test Blog by Cypress added')
     })
+    it('A blog can be liked', function () {
+      cy.contains('new blog').click()
+      cy.get('#blog_title').type('Cypress Test Blog')
+      cy.get('#blog_author').type('Cypress')
+      cy.get('#blog_url').type('www.cypress.com')
+      cy.contains('submit').click()
+      cy.contains('view').click()
+      cy.contains('like').click()
+      cy.contains('Likes: 1')
+    })
+    it('A blog can be deleted', function () {
+      cy.contains('new blog').click()
+      cy.get('#blog_title').type('Cypress Test Blog')
+      cy.get('#blog_author').type('Cypress')
+      cy.get('#blog_url').type('www.cypress.com')
+      cy.contains('submit').click()
+      cy.contains('view').click()
+      cy.contains('Remove').click()
+      cy.contains('Blog Cypress Test Blog by Cypress deleted')
+    })
+    it('A blog can only be deleted by creator', function () {
+      cy.contains('new blog').click()
+      cy.get('#blog_title').type('Cypress Test Blog')
+      cy.get('#blog_author').type('Cypress')
+      cy.get('#blog_url').type('www.cypress.com')
+      cy.contains('submit').click()
+      cy.contains('Logout').click()
+      const user = {
+        name: 'Jane Doe',
+        username: 'UnkownUser2',
+        password: 'pass'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'UnkownUser2', password: 'pass'
+      }).then(response => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
+      cy.contains('view').click()
+      cy.contains('Remove').should('not.exist')
+    })
   })
 })
