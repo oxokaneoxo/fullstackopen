3e66ba26a41578f02b3088a10e3b2299a9db6795
oxokaneoxo <kane.miller@hotmail.com>
2023-04-05 16:53:48 -0700

    5.23: bloglist end to end testing, step7

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-undef */
 describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'John Doe',
       username: 'UnkownUser',
       password: 'pass'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
   })
 
   it('Login form is shown', function () {
@@ -37,12 +37,7 @@ describe('Blog app', function () {
 
   describe('When logged in', function () {
     beforeEach(function () {
-        cy.request('POST', 'http://localhost:3003/api/login', {
-        username: 'UnkownUser', password: 'pass'
-      }).then(response => {
-        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
-        cy.visit('http://localhost:3000')
-      })
+      cy.login({ username: 'UnkownUser', password: 'pass' })
     })
 
     it('A blog can be created', function () {
@@ -53,16 +48,18 @@ describe('Blog app', function () {
       cy.contains('submit').click()
       cy.contains('a new blog Cypress Test Blog by Cypress added')
     })
+
     it('A blog can be liked', function () {
-      cy.contains('new blog').click()
+      cy.get('#add-blog_button').click()
       cy.get('#blog_title').type('Cypress Test Blog')
       cy.get('#blog_author').type('Cypress')
       cy.get('#blog_url').type('www.cypress.com')
       cy.contains('submit').click()
       cy.contains('view').click()
-      cy.contains('like').click()
+      cy.get('.like_button').click()
       cy.contains('Likes: 1')
     })
+
     it('A blog can be deleted', function () {
       cy.contains('new blog').click()
       cy.get('#blog_title').type('Cypress Test Blog')
@@ -73,6 +70,7 @@ describe('Blog app', function () {
       cy.contains('Remove').click()
       cy.contains('Blog Cypress Test Blog by Cypress deleted')
     })
+
     it('A blog can only be deleted by creator', function () {
       cy.contains('new blog').click()
       cy.get('#blog_title').type('Cypress Test Blog')
@@ -85,15 +83,38 @@ describe('Blog app', function () {
         username: 'UnkownUser2',
         password: 'pass'
       }
-      cy.request('POST', 'http://localhost:3003/api/users/', user)
-      cy.request('POST', 'http://localhost:3003/api/login', {
-        username: 'UnkownUser2', password: 'pass'
-      }).then(response => {
-        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
-        cy.visit('http://localhost:3000')
-      })
+      cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+      cy.login({ username: 'UnkownUser2', password: 'pass' })
       cy.contains('view').click()
       cy.contains('Remove').should('not.exist')
+    })
+
+    describe('and multiple blogs exists', function () {
+      beforeEach(function () {
+        const blogs = [{
+          title: 'Middle of the pack blog',
+          author: 'First creator',
+          url: 'https://www.cypress.com',
+          likes: 25,
+        }, {
+          title: 'Least liked blog',
+          author: 'Second creator',
+          url: 'https://www.cypress.com',
+          likes: 10,
+        }, {
+          title: 'Most liked blog',
+          author: 'Last creator',
+          url: 'https://www.cypress.com',
+          likes: 50,
+        }]
+        blogs.forEach((blog) => cy.createBlog(blog))
+      })
+
+      it('blogs are ordered by the number of likes (highest first)', function () {
+        cy.get('.blog_container').eq(0).should('contain', 'Most liked blog')
+        cy.get('.blog_container').eq(1).should('contain', 'Middle of the pack blog')
+        cy.get('.blog_container').eq(2).should('contain', 'Least liked blog')
+      })
     })
   })
 })
